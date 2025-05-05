ee912648a118a586ae1adc62161775394ab2593a
FrazierMark <m.mark.frazier@gmail.com>
2025-05-05 11:52:03 -0700

    Initial commit

---
+++ src/features/pagesSlice.js
@@ -0,0 +1,58 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// Import the pictures array and pages setup from UI.jsx
+const pictures = [
+	'DSC00680',
+	'DSC00933',
+	'DSC00966',
+	'DSC00983',
+	'DSC01011',
+	'DSC01040',
+	'DSC01064',
+	'DSC01071',
+	'DSC01103',
+	'DSC01145',
+	'DSC01420',
+	'DSC01461',
+	'DSC01489',
+	'DSC02031',
+	'DSC02064',
+	'DSC02069',
+];
+
+export const pages = [
+	{
+		front: 'book-cover',
+		back: pictures[0],
+	},
+];
+
+for (let i = 1; i < pictures.length - 1; i += 2) {
+	pages.push({
+		front: pictures[i % pictures.length],
+		back: pictures[(i + 1) % pictures.length],
+	});
+}
+
+pages.push({
+	front: pictures[pictures.length - 1],
+	back: 'book-back',
+});
+
+const pagesSlice = createSlice({
+	name: 'pages',
+	initialState: {
+		currentPage: 0,
+		pages: pages,
+	},
+	reducers: {
+		setPage: (state, action) => {
+			state.currentPage = action.payload;
+		},
+	},
+});
+
+export const { setPage } = pagesSlice.actions;
+export const selectCurrentPage = (state) => state.pages.currentPage;
+export const selectPages = (state) => state.pages.pages;
+export default pagesSlice.reducer;
