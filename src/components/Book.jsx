e95ef42bcb71c2353a5e7017ce8674af8fbfd83f
FrazierMark <m.mark.frazier@gmail.com>
2025-05-06 15:46:00 -0700

    Page selector working

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,19 +1,16 @@
 import { Page } from "./Page";
 import { useSelector } from "react-redux";
-import { allPages } from "../features/pagesSlice";
+import { allPages, selectCurrentPage } from "../features/pagesSlice";
 
 export const Book = ({ ...props }) => {
   const pages = useSelector(allPages);
+  const currentPage = useSelector(selectCurrentPage);
 
   return (
-    <group {...props}>
+    <group {...props} rotation-y={-Math.PI / 2}>
       {
         [...pages].map((pageData, index) => (
-          index === 0 ? (
-            <Page position-x={index * 0.15} key={index} number={index} {...pageData} />
-          ) : (
-            null
-          )
+          <Page key={index} page={currentPage} number={index} opened={currentPage > index} {...pageData} />
         ))
       }
     </group>
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,5 +1,11 @@
-import { useRef, useMemo } from "react";
+import { useRef, useMemo, useEffect } from "react";
 import { BoxGeometry, MeshStandardMaterial, SkinnedMesh, Bone, Skeleton, Vector3, Uint16BufferAttribute, Float32BufferAttribute, Color } from "three";
+import { useHelper, useTexture } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useSelector } from "react-redux";
+import { allPages } from "../features/pagesSlice";
+import { degToRad } from "three/src/math/MathUtils";
+import { SkeletonHelper, SRGBColorSpace } from "three";
 
 const PAGE_WIDTH = 1.28;
 const PAGE_HEIGHT = 1.71; //4:3 aspect ratio
@@ -62,15 +68,28 @@ const pageMaterials = [
   new MeshStandardMaterial({
     color: whiteColor,
   }),
-  new MeshStandardMaterial({
-    color: "pink",
-  }),
-  new MeshStandardMaterial({
-    color: "blue",
-  }),
 ]
 
-export const Page = ({ number, front, back, ...props }) => {
+export const Page = ({ number, front, back, page, opened, ...props }) => {
+  const pages = useSelector(allPages);
+
+  // Preload textures when component mounts
+  useEffect(() => {
+    pages.forEach(page => {
+      useTexture.preload(`/textures/${page.front}.jpg`);
+      useTexture.preload(`/textures/${page.back}.jpg`);
+      useTexture.preload(`/textures/book-cover-roughness.jpg`);
+    });
+  }, [pages]);
+
+  const [picture, picture2, pictureRoughness] = useTexture([
+    `/textures/${front}.jpg`,
+    `/textures/${back}.jpg`,
+    ...(number === 0 || number === pages.length - 1) ? [`/textures/book-cover-roughness.jpg`] : []
+  ]);
+
+  picture.colorSpace = picture2.colorSpace = SRGBColorSpace;
+
   const groupRef = useRef();
   const skinnedMeshRef = useRef();
 
@@ -92,7 +111,28 @@ export const Page = ({ number, front, back, ...props }) => {
     const skeleton = new Skeleton(bones);
 
     // Create the skinned mesh
-    const mesh = new SkinnedMesh(pageGeometry, pageMaterials);
+    const materials = [...pageMaterials,
+    new MeshStandardMaterial({
+      color: whiteColor,
+      map: picture,
+      ...(number === 0) ? {
+        roughnessMap: pictureRoughness,
+      } : {
+        roughness: 0.1 // closer to 1 is more matte
+      }
+    }),
+    new MeshStandardMaterial({
+      color: whiteColor,
+      map: picture2,
+      ...(number === pages.length - 1) ? {
+        roughnessMap: pictureRoughness,
+      } : {
+        roughness: 0.1 // closer to 1 is more matte
+      }
+    }),
+    ];
+
+    const mesh = new SkinnedMesh(pageGeometry, materials);
     mesh.castShadow = true;
     mesh.receiveShadow = true;
     mesh.frustumCulled = false;
@@ -107,9 +147,25 @@ export const Page = ({ number, front, back, ...props }) => {
     return mesh;
   }, []);
 
+  useHelper(skinnedMeshRef, SkeletonHelper, "red");
+  useFrame(() => {
+    if (!skinnedMeshRef.current) return;
+
+
+    let targetRotation = opened ? -Math.PI / 2 : Math.PI / 2;
+    targetRotation += degToRad(number * 0.5)
+
+    const bones = skinnedMeshRef.current.skeleton.bones;
+    bones[0].rotation.y = targetRotation;
+  });
+
   return (
     <group {...props} ref={groupRef}>
-      <primitive object={manualSkinnedMesh} ref={skinnedMeshRef} />
+      <primitive
+        object={manualSkinnedMesh}
+        ref={skinnedMeshRef}
+        position-z={-number * PAGE_DEPTH + page * PAGE_DEPTH}
+      />
     </group>
   )
 }
